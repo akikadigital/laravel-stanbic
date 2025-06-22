0853e1c958b975028a64fcfc850a49f1d690c150
Timothy Karani <kartimothy@gmail.com>
2025-06-23 00:58:53 +0300

    feat: done with GroupHeader popo

--- src/Data/ValueObjects/GroupHeader.php
+++ src/Data/ValueObjects/GroupHeader.php
@@ -6,7 +6,15 @@ use ValueError;
 
 class GroupHeader extends XmlValueObject
 {
-    public ?ControlSum $controlSum;
+    public ?MessageId $messageId = null;
+
+    public ?CreationDateTime $creationDateTime = null;
+
+    public ?NumberOfTransactions $numberOfTransactions = null;
+
+    public ?ControlSum $controlSum = null;
+
+    public ?InitiatingParty $initiatingParty = null;
 
     public function __construct() {}
 
@@ -18,13 +26,23 @@ class GroupHeader extends XmlValueObject
     /** @return array<string, array<string, mixed>> */
     public function getElement(): array
     {
-        if (! $this->controlSum) {
+        if (
+            ! $this->messageId ||
+            ! $this->creationDateTime ||
+            ! $this->numberOfTransactions ||
+            ! $this->controlSum ||
+            ! $this->initiatingParty
+        ) {
             throw new ValueError;
         }
 
         return [
             $this->getName() => [
+                ...$this->messageId->getElement(),
+                ...$this->creationDateTime->getElement(),
+                ...$this->numberOfTransactions->getElement(),
                 ...$this->controlSum->getElement(),
+                ...$this->initiatingParty->getElement(),
             ],
         ];
     }
--- tests/Data/ValueObjects/GroupHeaderTest.php
+++ tests/Data/ValueObjects/GroupHeaderTest.php
@@ -3,7 +3,11 @@
 namespace Akika\LaravelStanbic\Tests\Data\ValueObjects;
 
 use Akika\LaravelStanbic\Data\ValueObjects\ControlSum;
+use Akika\LaravelStanbic\Data\ValueObjects\CreationDateTime;
 use Akika\LaravelStanbic\Data\ValueObjects\GroupHeader;
+use Akika\LaravelStanbic\Data\ValueObjects\InitiatingParty;
+use Akika\LaravelStanbic\Data\ValueObjects\MessageId;
+use Akika\LaravelStanbic\Data\ValueObjects\NumberOfTransactions;
 use Akika\LaravelStanbic\Tests\TestCase;
 
 class GroupHeaderTest extends TestCase
@@ -11,13 +15,22 @@ class GroupHeaderTest extends TestCase
     public function test_element_has_correct_properties(): void
     {
         $header = new GroupHeader;
+        $header->messageId = new MessageId(fake()->uuid());
+        $header->creationDateTime = new CreationDateTime(now());
+        $header->numberOfTransactions = new NumberOfTransactions(1);
         $header->controlSum = new ControlSum(100);
+        $header->initiatingParty = new InitiatingParty(fake()->word(), fake()->uuid());
 
         $this->assertEquals('GrpHdr', $header->getName());
 
         $expected = ['GrpHdr' => [
-            'CtrlSum' => 100,
+            ...$header->messageId->getElement(),
+            ...$header->creationDateTime->getElement(),
+            ...$header->numberOfTransactions->getElement(),
+            ...$header->controlSum->getElement(),
+            ...$header->initiatingParty->getElement(),
         ]];
+
         $this->assertEquals($expected, $header->getElement());
     }
 }
