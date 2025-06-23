cb4f5091f227042ada5a1488b3b94db199c78cf4
Timothy Karani <kartimothy@gmail.com>
2025-06-23 14:15:57 +0300

    chore: refactored sample file access

--- tests/Data/ValueObjects/Reports/Pain00200103Test.php
+++ tests/Data/ValueObjects/Reports/Pain00200103Test.php
@@ -4,11 +4,13 @@ namespace Akika\LaravelStanbic\Tests\Data\ValueObjects\Reports;
 
 use Akika\LaravelStanbic\Data\ValueObjects\Reports\Pain00200103;
 use Akika\LaravelStanbic\Enums\GroupStatusType;
+use Akika\LaravelStanbic\Tests\HasSampleFiles;
 use Akika\LaravelStanbic\Tests\TestCase;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 class Pain00200103Test extends TestCase
 {
+    use HasSampleFiles;
+
     public function test_can_parse_xml(): void
     {
         $report = Pain00200103::fromXml($this->ackReport());
@@ -30,29 +32,5 @@ class Pain00200103Test extends TestCase
         $report = Pain00200103::fromXml($this->invalidAccountNoReport());
 
         $this->assertEquals('1000075215', $report->originalPaymentInfoAndStatus->originalPaymentInfoId);
-    }
-
-    private function ackReport(): string
-    {
-        return $this->getFileContents('ack.xml');
-    }
-
-    private function nackReport(): string
-    {
-        return $this->getFileContents('nack.xml');
-    }
-
-    private function invalidAccountNoReport(): string
-    {
-        return $this->getFileContents('invalid_account_no.xml');
-    }
-
-    public function getFileContents(string $path): string
-    {
-        if (! $contents = file_get_contents(__DIR__."/samples/{$path}")) {
-            throw new FileNotFoundException;
-        }
-
-        return $contents;
     }
 }
---
+++ tests/HasSampleFiles.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Akika\LaravelStanbic\Tests;
+
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+
+trait HasSampleFiles
+{
+    public function ackReport(): string
+    {
+        return $this->getFileContents('ack.xml');
+    }
+
+    public function nackReport(): string
+    {
+        return $this->getFileContents('nack.xml');
+    }
+
+    public function invalidAccountNoReport(): string
+    {
+        return $this->getFileContents('invalid_account_no.xml');
+    }
+
+    public function basePath(string $path = ''): string
+    {
+        $orchestraPath = '/vendor/orchestra/testbench-core/laravel';
+        $root = str_replace($orchestraPath, '', base_path());
+
+        return $root."/{$path}";
+    }
+
+    public function getFileContents(string $path): string
+    {
+        $fullPath = $this->basePath("tests/samples/{$path}");
+
+        if (! $contents = file_get_contents($fullPath)) {
+            throw new FileNotFoundException;
+        }
+
+        return $contents;
+    }
+}
