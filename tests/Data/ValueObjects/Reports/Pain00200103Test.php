eca06a88e8806331b4af4755e67a5d961bf6eac1
Timothy Karani <kartimothy@gmail.com>
2025-06-23 12:53:24 +0300

    chore: moved test xml to disk

--- tests/Data/ValueObjects/Reports/Pain00200103Test.php
+++ tests/Data/ValueObjects/Reports/Pain00200103Test.php
@@ -5,12 +5,13 @@ namespace Akika\LaravelStanbic\Tests\Data\ValueObjects\Reports;
 use Akika\LaravelStanbic\Data\ValueObjects\Reports\Pain00200103;
 use Akika\LaravelStanbic\Enums\GroupStatusType;
 use Akika\LaravelStanbic\Tests\TestCase;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 class Pain00200103Test extends TestCase
 {
     public function test_can_parse_xml(): void
     {
-        $report = Pain00200103::fromXml(self::ACK);
+        $report = Pain00200103::fromXml($this->ackReport());
 
         $expectedOrgMsgId = 'DPTS001';
         $orgMsgId = $report->xmlReader->value('CstmrPmtStsRpt.OrgnlGrpInfAndSts.OrgnlMsgId')->sole();
@@ -19,86 +20,39 @@ class Pain00200103Test extends TestCase
 
     public function test_can_get_group_status(): void
     {
-        $report = Pain00200103::fromXml(self::ACK);
+        $report = Pain00200103::fromXml($this->nackReport());
 
-        $this->assertEquals(GroupStatusType::Rcvd, $report->getGroupStatus());
+        $this->assertEquals(GroupStatusType::Rjct, $report->getGroupStatus());
     }
 
     public function test_can_get_original_payment_info(): void
     {
-        $invalidAccountNumberPath = __DIR__.'/samples/invalid_account_no.xml';
-        $content = file_get_contents($invalidAccountNumberPath);
-        $report = Pain00200103::fromXml($content);
+        $report = Pain00200103::fromXml($this->invalidAccountNoReport());
 
         $this->assertEquals('1000075215', $report->originalPaymentInfoAndStatus->originalPaymentInfoId);
     }
 
-    public const ACK = "<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
-<Document xmlns='urn:iso:std:iso:20022:tech:xsd:pain.002.001.03' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'>
-  <CstmrPmtStsRpt>
-    <GrpHdr>
-      <MsgId>20027226</MsgId>
-      <CreDtTm>2018-01-26T09:59:45</CreDtTm>
-      <InitgPty>
-        <Nm>Standard Bank SA</Nm>
-        <Id>
-          <OrgId>
-            <BICOrBEI>SBZAZAJJXXX</BICOrBEI>
+    private function ackReport(): string
+    {
+        return $this->getFileContents('ack.xml');
+    }
 
-          </OrgId>
-        </Id>
-      </InitgPty>
-    </GrpHdr>
+    private function nackReport(): string
+    {
+        return $this->getFileContents('nack.xml');
+    }
 
-    <OrgnlGrpInfAndSts>
-      <OrgnlMsgId>DPTS001</OrgnlMsgId>
+    private function invalidAccountNoReport(): string
+    {
+        return $this->getFileContents('invalid_account_no.xml');
+    }
 
-      <OrgnlMsgNmId>PAIN.001.001.03</OrgnlMsgNmId>
-      <OrgnlCreDtTm>2018-01-26T18:40:59</OrgnlCreDtTm>
-      <OrgnlNbOfTxs>1</OrgnlNbOfTxs>
+    public function getFileContents(string $path): string
+    {
+        if (! $contents = file_get_contents(__DIR__."/samples/{$path}")) {
+            throw new FileNotFoundException;
+        }
 
-      <OrgnlCtrlSum>10.0</OrgnlCtrlSum>
-      <GrpSts>RCVD</GrpSts>
-    </OrgnlGrpInfAndSts>
-
-  </CstmrPmtStsRpt>
-</Document>
-    ";
-
-    public const NACK = "<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
-<Document xmlns='urn:iso:std:iso:20022:tech:xsd:pain.002.001.03' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'>
-  <CstmrPmtStsRpt>
-    <GrpHdr>
-      <MsgId>20027490</MsgId>
-      <CreDtTm>2018-01-26T11:00:04</CreDtTm>
-      <InitgPty>
-        <Nm>Standard Bank SA</Nm>
-        <Id>
-          <OrgId>
-            <BICOrBEI>SBZAZAJJXXX</BICOrBEI>
-
-          </OrgId>
-        </Id>
-      </InitgPty>
-    </GrpHdr>
-
-    <OrgnlGrpInfAndSts>
-      <OrgnlMsgId>DPTS001</OrgnlMsgId>
-
-      <OrgnlMsgNmId>PAIN.001.001.03</OrgnlMsgNmId>
-      <OrgnlCreDtTm>2018-01-26T18:40:59</OrgnlCreDtTm>
-      <OrgnlNbOfTxs>1</OrgnlNbOfTxs>
-
-      <OrgnlCtrlSum>10.0</OrgnlCtrlSum>
-      <GrpSts>RJCT</GrpSts>
-      <StsRsnInf>
-        <Rsn>
-          <Cd>NARR</Cd>
-        </Rsn>
-        <AddtlInf>Duplicate File</AddtlInf>
-      </StsRsnInf>
-    </OrgnlGrpInfAndSts>
-  </CstmrPmtStsRpt>
-</Document>
-";
+        return $contents;
+    }
 }
