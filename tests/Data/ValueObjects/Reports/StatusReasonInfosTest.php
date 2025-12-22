b3f3f2843a407f78d3e30dc2e2d07d016831d896
Timothy Karani <kartimothy@gmail.com>
2025-12-22 14:25:47 +0300

    test: cover Reports/StatusReasonInfos

---
+++ tests/Data/ValueObjects/Reports/StatusReasonInfosTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Akika\LaravelStanbic\Tests\Data\ValueObjects\Reports;
+
+use Akika\LaravelStanbic\Data\ValueObjects\Reports\StatusReasonInfos;
+use Akika\LaravelStanbic\Tests\HasSampleFiles;
+use Akika\LaravelStanbic\Tests\TestCase;
+use Saloon\XmlWrangler\XmlReader;
+
+class StatusReasonInfosTest extends TestCase
+{
+    use HasSampleFiles;
+
+    public XmlReader $xmlReader;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $xml = $this->invalidAccountNoReport();
+        $this->xmlReader = XmlReader::fromString($xml);
+    }
+
+    public function test_can_parse_from_xml(): void
+    {
+        $root = '//CstmrPmtStsRpt/OrgnlGrpInfAndSts';
+
+        $statusReasonInfos = StatusReasonInfos::fromXmlReader($this->xmlReader, $root);
+
+        /** @var \Illuminate\Support\Collection<int, string> */
+        $infos = $this->xmlReader->xpathValue("{$root}/StsRsnInf/AddtlInf")->collect();
+
+        $this->assertEquals($infos->all(), $statusReasonInfos->infos->all());
+    }
+}
